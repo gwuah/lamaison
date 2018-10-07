adc6832fb38f24bc5ce9bb2449537232f653c4c7
griffith <griffithawuah15@gmail.com>
2018-10-07 13:52:02 +0000

    added landing page, converted to vue

--- public/static/js/index.js
+++ public/static/js/index.js
@@ -1,1 +1,13 @@
-console.log('loool');
+
+const cards = document.querySelectorAll('.card');
+
+function transition() {
+  if (this.classList.contains('active')) {
+    this.classList.remove('active')
+  } else {
+    this.classList.add('active');
+  }
+}
+
+cards.forEach(card => card.addEventListener('click', transition));
+ 
