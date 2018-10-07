81c2e1d3088232b3007034c7ff839af9f17ce38d
griffith <griffithawuah15@gmail.com>
2018-10-07 15:35:14 +0000

    setup authentication

--- routes/api/index.js
+++ routes/api/index.js
@@ -1,7 +1,19 @@
+const {authenticate} = require('../../middlewares/auth');
+const {wrapAsync} = require('../../utilities');
 const express = require('express');
 const router = new express.Router();
 const v1Router = require('./v1');
 
-router.use('/v1', v1Router);
+const whiteList = [{
+  method: 'POST',
+  url: '/owners',
+  clean: true
+}];
+
+const authenticationMiddleware = authenticate({
+  whiteList
+});
+
+router.use('/v1', wrapAsync(authenticationMiddleware), v1Router);
 
 module.exports = router;
