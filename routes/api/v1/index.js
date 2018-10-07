aee553efbc8c7aa08ec046d2d27ff1a1ab1c52c0
griffith <griffithawuah15@gmail.com>
2018-10-07 15:37:00 +0000

    plugin authentication

--- routes/api/v1/index.js
+++ routes/api/v1/index.js
@@ -1,16 +1,22 @@
 const housingRoutes = require('./Housing');
-const ownerRoutes = require('./Owner')
+const ownerRoutes = require('./Owner');
+// setup authorization here
+const {authorize} = require('../../../middlewares/auth');
+const {wrapAsync} = require('../../../utilities');
+
 
 const express = require('express');
 const router = new express.Router();
 
 router.use(
   '/housing', 
+  wrapAsync(authorize({name: 'HOUSING'})),
   housingRoutes
 );
 
 router.use(
-  '/owners', 
+  '/owners',
+  wrapAsync(authorize({name: 'OWNERS'})),
   ownerRoutes
 );
 
